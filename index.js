01e5759f61e7707844af1038170f2ae056b7a7f6
Bessmack <jenwied4lif@gmail.com>
2025-06-10 01:04:41 +0300

    complete loop lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(names) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+
+function countDown(num){
+    let i = 0;
+    while (num >= i){
+        console.log(num--);
+    }
+}
+countDown(10);
